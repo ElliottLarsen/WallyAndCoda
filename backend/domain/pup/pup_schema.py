d52d5982d14f525774fa577edba263b4c38856b3
Elliott Larsen <elliottlrsn@gmail.com>
2023-12-20 20:56:46 -0700

    Pup schema added.

---
+++ backend/domain/pup/pup_schema.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, validator
+
+
+class PupCreate(BaseModel):
+    pup_name: str
+    pup_sex: str
+    microchip_number: str
+    akc_registration_number: str
+    akc_registration_name: str
+
+    @validator("pup_name", "pup_sex", "microchip_number")
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("This is a required field.")
+        return v
+
+
+class PupUpdate(BaseModel):
+    pup_name: str
+    pup_sex: str
+    microchip_number: str
+    akc_registration_number: str
+    akc_registration_name: str
+
+    @validator("pup_name", "pup_sex", "microchip_number")
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("This is a required field.")
+        return v
+
+
+class PupResponse(BaseModel):
+    id: str
+    pup_name: str
+    pup_sex: str
+    microchip_number: str
+    akc_registration_number: str
+    akc_registration_name: str
