6a4ebaf4d1db60a819218c100608e43f54b27509
Elliott Larsen <elliottlrsn@gmail.com>
2023-12-20 21:19:00 -0700

    Pup CRUD complete.

--- backend/domain/pup/pup_crud.py
+++ backend/domain/pup/pup_crud.py
@@ -32,6 +32,33 @@ def create_pup(
     return get_pup_by_name(db, pup_create.pup_name)
 
 
+def update_pup(
+    db: Session,
+    pup_update: PupUpdate,
+    pup: Pup,
+) -> Pup:
+    """
+    Update pup
+    """
+    pup = get_pup_by_id(db, pup.id)
+    pup.pup_name = pup_update.pup_name
+    pup.pup_sex = pup_update.pup_sex
+    pup.microchip_number = pup_update.microchip_number
+    pup.akc_registration_number = pup_update.akc_registration_number
+    pup.akc_registration_name = pup_update.akc_registration_name
+    db.add(pup)
+    db.commit()
+    return get_pup_by_id(pup.id)
+
+
+def remove_pup(
+    db: Session,
+    pup: Pup,
+) -> None:
+    db.delete(pup)
+    db.commit()
+
+
 def get_pup_by_name(
     db: Session,
     pup_name: str,
@@ -40,3 +67,13 @@ def get_pup_by_name(
     Retrieves a pup by the given name
     """
     return db.query(Pup).filter(Pup.pup_name == pup_name).first()
+
+
+def get_pup_by_id(
+    db: Session,
+    id: str,
+) -> Pup | None:
+    """
+    Retrieves a pup by the given name
+    """
+    return db.query(Pup).filter(Pup.id == id).first()
