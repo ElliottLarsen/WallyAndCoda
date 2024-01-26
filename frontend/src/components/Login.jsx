9a17c691d8c6854ebd4fdce9f8f95bbf35f6485b
Elliott Larsen <elliottlrsn@gmail.com>
2024-01-25 21:06:34 -0700

    Login form added.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,3 +1,15 @@
 export default function Login() {
-    return 
+    return (
+        <form action="/login" method="POST">
+            <p>
+                <label htmlFor="username">Username: </label>
+                <input id="username" type="text" placeholder="username" required />
+            </p>
+            <p>
+                <label htmlFor="password">Password: </label>
+                <input id="password" type="password" placeholder="password" required />
+            </p>
+            <input type="submit" value="Login" />
+        </form>
+    )
 }
