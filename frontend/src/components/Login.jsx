ef742a1e02fadea36cc41ff6ed1d681afba27c43
Elliott Larsen <elliottlrsn@gmail.com>
2024-01-26 19:08:25 -0700

    React form added for login.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 export default function Login() {
+
+    const [loginData, setloginData] = useState({username: "", password: ""});
+    
+    const handleChange = (evt) => {
+        const changedField = evt.target.name;
+        const newValue = evt.target.value;
+        setloginData(currData => {
+            currData[changedField] = newValue;
+            return{...currData};
+        })
+    }
+
+    const handleLogin = () => {
+        console.log(loginData);
+    }
+
     return (
-        <form action="/login" method="POST">
+        <div>
             <p>
                 <label htmlFor="username">Username: </label>
-                <input id="username" type="text" placeholder="username" required />
+                <input id="username" type="text" placeholder="username" name="username" value={loginData.username} onChange={handleChange} required />
             </p>
             <p>
                 <label htmlFor="password">Password: </label>
-                <input id="password" type="password" placeholder="password" required />
+                <input id="password" type="password" placeholder="password" name="password" value={loginData.password} onChange={handleChange} required />
             </p>
-            <input type="submit" value="Login" />
-        </form>
+            <button onClick={handleLogin}>Login</button>
+        </div>
     )
 }
