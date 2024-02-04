ec6774a0d8fe8c026ddabbd546c090ef925a6fd8
Elliott Larsen <elliottlrsn@gmail.com>
2024-02-03 20:34:53 -0700

    Record and reminder models added.

--- backend/domain/pup/pup_schema.py
+++ backend/domain/pup/pup_schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, validator
 from models import User
+from datetime import datetime
 
 
 class PupCreate(BaseModel):
@@ -38,3 +39,75 @@ class PupResponse(BaseModel):
     akc_registration_number: str
     akc_registration_name: str
     owner_id: str
+
+
+class RecordCreate(BaseModel):
+    record_type: str
+    record_date: datetime
+    doctor_name: str
+    vet_address: str
+    vet_phone_number: str
+    cost: float
+    record_note: str
+
+    @validator(
+        "record_type",
+        "record_date",
+        "doctor_name",
+        "vet_address",
+        "vet_phone_number",
+        "cost",
+        "record_note",
+    )
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("This is a required field.")
+        return v
+
+
+class RecordUpdate(BaseModel):
+    record_type: str
+    record_date: datetime
+    doctor_name: str
+    vet_address: str
+    vet_phone_number: str
+    cost: float
+    record_note: str
+
+    @validator(
+        "record_type",
+        "record_date",
+        "doctor_name",
+        "vet_address",
+        "vet_phone_number",
+        "cost",
+        "record_note",
+    )
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("This is a required field.")
+        return v
+
+
+class ReminderCreate(BaseModel):
+    reminder_date: datetime
+    reminder_note: str
+    completed: bool
+
+    @validator("reminder_date", "reminder_note", "completed")
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("This is a required field.")
+        return v
+
+
+class ReminderUpdate(BaseModel):
+    reminder_date: datetime
+    reminder_note: str
+    completed: bool
+
+    @validator("reminder_date", "reminder_note", "completed")
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError("This is a required field.")
+        return v
