02d05eda703568eb57d94e61bae3d97347f4c3a2
Elliott Larsen <elliottlrsn@gmail.com>
2024-02-06 20:28:51 -0700

    User registration tests.

--- backend/test_app/test_user_endpoints.py
+++ backend/test_app/test_user_endpoints.py
@@ -16,6 +16,11 @@ def test_user():
     return {"username": "testuser", "password": "testpassword"}
 
 
+@pytest.fixture
+def test_user2():
+    return {"username": "testuser2", "password": "testpassword2"}
+
+
 def test_user_account_get_401():
     """
     Unauthorized access
@@ -72,3 +77,56 @@ def test_user_account_delete_by_id_401():
     """
     response = client_401.delete("/wallyandcoda/user/eerd334872-34")
     assert response.status_code == 401
+
+
+def test_user_register(client):
+    """
+    User registration endpoint test
+    """
+    data = {
+        "username": "testuser",
+        "password1": "testpassword",
+        "password2": "testpassword",
+        "first_name": "TEST",
+        "last_name": "USER",
+        "email": "testuser@testuser.com",
+    }
+    response = client.post("/wallyandcoda/user/register", json=data)
+    assert response.status_code == 200
+    assert "id" in response.json()
+    assert response.json()["username"] == "testuser"
+    assert response.json()["first_name"] == "TEST"
+    assert response.json()["last_name"] == "USER"
+    assert response.json()["email"] == "testuser@testuser.com"
+
+
+def test_user_duplicate_username_register(client):
+    """
+    User duplicate registration endpoint test
+    """
+    data = {
+        "username": "testuser",
+        "password1": "testpassword",
+        "password2": "testpassword",
+        "first_name": "TEST",
+        "last_name": "USER",
+        "email": "testuser1@testuser.com",
+    }
+    response = client.post("/wallyandcoda/user/register", json=data)
+    assert response.status_code == 409
+
+
+def test_user_duplicate_email_register(client):
+    """
+    User duplicate registration endpoint test
+    """
+    data = {
+        "username": "testuser1",
+        "password1": "testpassword",
+        "password2": "testpassword",
+        "first_name": "TEST",
+        "last_name": "USER",
+        "email": "testuser@testuser.com",
+    }
+    response = client.post("/wallyandcoda/user/register", json=data)
+    assert response.status_code == 409
