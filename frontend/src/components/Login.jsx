ce5104897a3edd07dea0d3356d480179c4d70acd
Elliott Larsen <elliottlrsn@gmail.com>
2024-02-18 19:42:18 -0700

    Login CSS added.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import '../styles/login.css'
 
 export default function Login() {
 
     return (
-        <form>
-            <label htmlFor="username">Username: </label>
-            <input id="username" type="text" placeholder="username" name="username" required />
-            <br />
-            <label htmlFor="password">Password: </label>
-            <input id="password" type="password" placeholder="password" name="password" required />
-            <br />
-            <button type="submit">Login</button>
-        </form>
+        <>
+            <div className='flex-container'>
+                <div className='square brown'>
+                    <hr/>
+                    <h1>Login</h1>
+                    <hr/>
+                </div>
+                <div className='square white'>
+                    <form>
+                        <label htmlFor="username">Username: </label>
+                        <input id="username" type="text" placeholder="username" name="username" required />
+                        <br />
+                        <label htmlFor="password">Password: </label>
+                        <input id="password" type="password" placeholder="password" name="password" required />
+                        <br />
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
