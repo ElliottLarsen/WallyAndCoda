0df0a5ccfeb02cbfdb32b21527dafaf5c36cf7a7
Josquin Larsen <jsqnlrsn@gmail.com>
2024-03-03 22:23:38 -0700

    Font, input fields, and required updates.

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -46,22 +46,22 @@ export default function Register() {
                 </div>
                 <div className='square white'>
                     <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username: </label>
-                        <input type="text" name="username" placeholder="username" id="username" onChange={handleChange} required />
+                        <label htmlFor="username" class="required">Username: </label>
+                        <input type="text" name="username" placeholder="username" id="username" onChange={handleChange} required autofocus />
                         <br />
-                        <label htmlFor="password1">Password: </label>
-                        <input type="password" name="password1" placeholder="password1" id="password1" onChange={handleChange} required />
+                        <label htmlFor="password1" class="required">Password: </label>
+                        <input type="password" name="password1" placeholder="password" id="password1" onChange={handleChange} required />
                         <br />
-                        <label htmlFor="password2">Confirm Password: </label>
-                        <input type="password" name="password2" placeholder="password2" id="password2" onChange={handleChange} required />
+                        <label htmlFor="password2" class="required">Confirm Password: </label>
+                        <input type="password" name="password2" placeholder="confirm password" id="password2" onChange={handleChange} required />
                         <br />
-                        <label htmlFor="first_name">First Name: </label>
+                        <label htmlFor="first_name" class="required">First Name: </label>
                         <input type="text" name="first_name" placeholder="first name" id="first_name" onChange={handleChange} required />
                         <br />
-                        <label htmlFor="last_name">Last Name: </label>
+                        <label htmlFor="last_name" class="required">Last Name: </label>
                         <input type="text" name="last_name" placeholder="last name" id="last_name" onChange={handleChange} required />
                         <br />
-                        <label htmlFor="email">Email: </label>
+                        <label htmlFor="email" class="required">Email: </label>
                         <input type="text" name="email" placeholder="email" id="email" onChange={handleChange} required />
                         <br />
                         <button type="submit">Register</button>
